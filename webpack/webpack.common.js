570d2e97a9ee60fd62aeab19cf020471e214cc70
christopher-caldwell <caldwell8814@gmail.com>
2020-08-23 08:45:03 -0500

    initial setup complete

---
+++ webpack/webpack.common.js
@@ -0,0 +1,45 @@
+const path = require('path')
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+module.exports = {
+  entry: path.resolve(process.cwd(), 'src/index.ts'),
+  output: {
+    path: path.resolve(process.cwd(), 'dist/'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-typescript'],
+            plugins: [
+              // new BundleAnalyzerPlugin()
+              'add-module-exports',
+              [
+                'module-resolver',
+                {
+                  root: ['./src'],
+                  alias: {
+                    '@': './src/',
+                  },
+                },
+              ],
+            ],
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  stats: 'minimal',
+  target: 'node',
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), 'src'),
+    },
+    extensions: ['.ts', '.js'],
+  },
+}
